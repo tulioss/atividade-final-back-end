b108c5a6c47278dcb5413f89b782cb58e39e939c
Joao Tulio Santos Coelho <tuliocolossos@gmail.com>
2024-07-21 18:51:10 -0300

    Correção na colocação de mesagens

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import {v4 as uuidv4} from 'uuid'
+import users from './users' 
 
 import {validationTitleDescription, updateValidation} from '../middlewares/validationsMessages'
 
